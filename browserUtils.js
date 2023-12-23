a420506007fc513fd8e181a1f97d0145e1d7b468
robertshum <robertshum@gmail.com>
2023-12-22 19:50:28 -0800

    add test cases for convertToJson fn.

--- browserUtils.js
+++ browserUtils.js
@@ -2,55 +2,60 @@ import puppeteer from "puppeteer";
 
 export async function createPageWithTimeout(timeout, endpoint, inputBrowser, userAgent, res) {
 
-    //another way to be fancy is to connect to BrightData (not free)
-    //BrightData allows use to use rotating proxies to evade bot detection
-    //see tutorial here - https://www.youtube.com/watch?v=qo_fUjb02ns&t=1s&ab_channel=BeyondFireship
-    //To connect to BrightData:
-    //USERNAME-ZONE:PASSWORD (format if we want to use BrightData proxy services)
+  //another way to be fancy is to connect to BrightData (not free)
+  //BrightData allows use to use rotating proxies to evade bot detection
+  //see tutorial here - https://www.youtube.com/watch?v=qo_fUjb02ns&t=1s&ab_channel=BeyondFireship
+  //To connect to BrightData:
+  //USERNAME-ZONE:PASSWORD (format if we want to use BrightData proxy services)
 
-    // const auth = 'USERNAME-ZONE:PASSWORD';
-    // browser = await puppeteer.connect({
-    //     //browser web socket endpoint
-    //     browserWSEndpoint: `wss://${auth}@location_of_proxy:port`
-    // });
+  // const auth = 'USERNAME-ZONE:PASSWORD';
+  // browser = await puppeteer.connect({
+  //     //browser web socket endpoint
+  //     browserWSEndpoint: `wss://${auth}@location_of_proxy:port`
+  // });
 
-    try {
-        let browser = inputBrowser;
+  try {
+    let browser = inputBrowser;
 
-        //in case it's closed for whatever reason
-        if (browser === null || browser === undefined || !browser.isConnected()) {
-            browser = createNewBrowser();
-        }
-
-        const page = await browser.newPage();
-        await page.setUserAgent(userAgent);
-        page.setDefaultNavigationTimeout(timeout);
-        await page.goto(endpoint);
-        return page;
-    } catch (error) {
-        console.error('Error in: createPageWithTimeout(...)', error);
-        return;
+    //in case it's closed for whatever reason
+    if (browser === null || browser === undefined || !browser.isConnected()) {
+      browser = createNewBrowser();
     }
+
+    const page = await browser.newPage();
+    await page.setUserAgent(userAgent);
+    page.setDefaultNavigationTimeout(timeout);
+    await page.goto(endpoint);
+    return page;
+  } catch (error) {
+    console.error('Error in: createPageWithTimeout(...)', error);
+    return;
+  }
 
 }
 
 export async function createNewBrowser() {
-    const browser = await puppeteer.launch({
-        headless: 'new',
-        //args: ['--proxy-server=192.241.189.47:31028']
-    });
-    return browser;
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    //args: ['--proxy-server=192.241.189.47:31028']
+  });
+  return browser;
 }
 
-export async function convertToJson(collectedData) {
-    //Remove duplicates of collectedData
-    const uniqueArray = collectedData.filter((obj, index, self) => {
-        // Compare objects based on a unique identifier or specific properties
-        // For example, if your objects have an 'id' property:
-        return index === self.findIndex((o) => o.title === obj.title);
-    });
-    uniqueArray.sort((a, b) => (a.price - b.price));
-    let jsonData = JSON.stringify(uniqueArray, null, 2);
-    jsonData = jsonData.trim();
-    return jsonData;
+export function convertToJson(collectedData) {
+
+  if (!collectedData || collectedData === null) {
+    return JSON.stringify([{}]);
+  }
+
+  //Remove duplicates of collectedData
+  const uniqueArray = collectedData.filter((obj, index, self) => {
+    // Compare objects based on a unique identifier or specific properties
+    // For example, if your objects have an 'id' property:
+    return index === self.findIndex((o) => o.title === obj.title);
+  });
+  uniqueArray.sort((a, b) => (a.price - b.price));
+  let jsonData = JSON.stringify(uniqueArray, null, 2);
+  jsonData = jsonData.trim();
+  return jsonData;
 }
--- tests/browserUtils.test.js
+++ tests/browserUtils.test.js
@@ -1,6 +1,131 @@
 import { convertToJson } from '../browserUtils';
 
-test('do stuff', () => {
-  // getting framework to play nice with ES6 first.
-  expect(true);
+describe('convertToJson function', () => {
+  test('Number of entries should not change after conversion', async () => {
+
+    const collectedData = [
+      {
+        title: "Pillsbuy Pizza Pockets",
+        price: 2.49,
+        link: 'https://www.example.com'
+      },
+      {
+        title: "Tide Detergent",
+        price: 2.49,
+        link: 'https://www.example.com'
+      },
+      {
+        title: "Pillsbuy Pizza Pockets 4 Cheese",
+        price: 2.49,
+        link: 'https://www.example.com'
+      }
+    ];
+
+    const resultingJsonString = convertToJson(collectedData);
+    const parsedObj = JSON.parse(resultingJsonString);
+    expect(Object.keys(parsedObj).length).toBe(3);
+  });
+
+  test('Remove duplicate entries.  3 should remain from 5', async () => {
+
+    const collectedData = [
+      {
+        title: "Pillsbuy Pizza Pockets",
+        price: 2.49,
+        link: 'https://www.example.com'
+      },
+      {
+        title: "Pillsbuy Pizza Pockets",
+        price: 3.49,
+        link: 'https://www.example.com/1'
+      },
+      {
+        title: "Tide Detergent",
+        price: 4.49,
+        link: 'https://www.example.com/2'
+      },
+      {
+        title: "Tide Detergent",
+        price: 5.49,
+        link: 'https://www.example.com/3'
+      },
+      {
+        title: "Pillsbuy Pizza Pockets 4 Cheese",
+        price: 2.49,
+        link: 'https://www.example.com'
+      }
+    ];
+
+    const resultingJsonString = convertToJson(collectedData);
+    const parsedObj = JSON.parse(resultingJsonString);
+    expect(Object.keys(parsedObj).length).toBe(3);
+  });
+
+  test('Items should be sorted from lowest price to highest', async () => {
+
+    const collectedData = [
+      {
+        title:
+          'Pillsbuy Pizza Pockets 4 Cheese',
+        price: 3.49,
+        link: 'https://www.example.com'
+      },
+      {
+        title:
+          'Pillsbuy Pizza Pockets - Pepperoni',
+        price: 2.49,
+        link: 'https://www.example.com/1'
+      },
+      {
+        title:
+          'Tide Detergent - Jumbo Super Wash',
+        price: 5.50,
+        link: 'https://www.example.com/2'
+      },
+      {
+        title:
+          'Tide Detergent - 24 pack Quick Dissolve Pods',
+        price: 5.49,
+        link: 'https://www.example.com/3'
+      }
+    ];
+
+    const resultingJsonString = convertToJson(collectedData);
+    const parsedObj = JSON.parse(resultingJsonString);
+    expect(Object.keys(parsedObj).length).toBe(4);
+
+    expect(parsedObj[0].title).toBe('Pillsbuy Pizza Pockets - Pepperoni');
+    expect(parsedObj[1].title).toBe('Pillsbuy Pizza Pockets 4 Cheese');
+    expect(parsedObj[2].title).toBe('Tide Detergent - 24 pack Quick Dissolve Pods');
+    expect(parsedObj[3].title).toBe('Tide Detergent - Jumbo Super Wash');
+  });
+
+  test('Return 1 entry (empty) if no data provided', async () => {
+
+    const collectedData = [{}];
+
+    const resultingJsonString = convertToJson(collectedData);
+    const parsedObj = JSON.parse(resultingJsonString);
+
+    // return [{}]
+    expect(Object.keys(parsedObj).length).toBe(1);
+    expect(parsedObj[0].title).toBeUndefined();
+    expect(parsedObj[0].price).toBeUndefined();
+    expect(parsedObj[0].link).toBeUndefined();
+  });
+
+  test('Return 1 entry (empty) if data is undefined', async () => {
+
+    const collectedData = undefined;
+
+    const resultingJsonString = convertToJson(collectedData);
+    const parsedObj = JSON.parse(resultingJsonString);
+    console.log(parsedObj);
+
+    // return [{}]
+    expect(Object.keys(parsedObj).length).toBe(1);
+    expect(parsedObj[0].title).toBeUndefined();
+    expect(parsedObj[0].price).toBeUndefined();
+    expect(parsedObj[0].link).toBeUndefined();
+  });
 });
