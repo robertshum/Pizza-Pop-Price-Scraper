52f1ab1bf7fd9b05f00786a1202429bf421b0e4e
robertshum <robertshum@gmail.com>
2024-01-03 15:17:23 -0800

    finish testcase for jim pattison food group

--- tests/processGroupData.test.js
+++ tests/processGroupData.test.js
@@ -18,12 +18,19 @@ describe('Testing Process Group Data', () => {
   // scraper logic for loblaws food group
   let loblawsGroupData;
 
+  // scraper logic for jim pattison food group
+  let jimPattisonFoodGroupData;
+
   beforeAll(async () => {
-    const { app, closeBrowser, processLoblawsGroupData } = await initApp();
+    const { app,
+      closeBrowser,
+      processLoblawsGroupData,
+      processJimPattisonFoodGroupData } = await initApp();
     appInstance = app;
     closeBrowserInstance = closeBrowser;
     listener = appInstance.listen(TESTING_PORT);
     loblawsGroupData = processLoblawsGroupData;
+    jimPattisonFoodGroupData = processJimPattisonFoodGroupData;
   });
 
   afterAll(async () => {
@@ -45,7 +52,7 @@ describe('Testing Process Group Data', () => {
     const directoryPath = path.dirname(currentFileUrl.pathname);
     const endpoint = `file://${directoryPath}/pages/loblaws_group.html`;
 
-    const site = 'https://www.example.com';
+    const site = 'https://www.superstore.com';
     const results = await loblawsGroupData(page, endpoint, site);
     let jsonData = JSON.parse(results.jsonData);
 
@@ -60,18 +67,53 @@ describe('Testing Process Group Data', () => {
     // note: the results should also be sorted by price, thats why we see 4.49 as the 2nd item and not the 1st. (3.49, 4.49, 5.49)
     expect(firstProduct.title).toBe('Pillsbury Pizza Pockets Pepperoni');
     expect(firstProduct.price).toBe(3.49);
-    expect(firstProduct.link).toBe(`${site}/pepperoni`)
+    expect(firstProduct.link).toBe(`${site}/pepperoni`);
 
     expect(secondProduct.title).toBe('Pillsbury Pizza Pockets 4 Cheese');
     expect(secondProduct.price).toBe(4.49);
-    expect(secondProduct.link).toBe(`${site}/4cheese`)
+    expect(secondProduct.link).toBe(`${site}/4cheese`);
 
     expect(thirdProduct.title).toBe('Pillsbury Pizza Pockets Supreme Deluxe');
     expect(thirdProduct.price).toBe(5.49);
-    expect(thirdProduct.link).toBe(`${site}/deluxe`)
+    expect(thirdProduct.link).toBe(`${site}/deluxe`);
   });
 
   test('processJimPattisonFoodGroupData should return product array related to Pillsbury', async () => {
-    // TODO
+
+    // call processJimPattisonFoodGroupData
+    // endpoint is the physical HTML file saved on file
+    // page is an undefined variable (will be defined in createPageWithTimeout)
+    // site is the root website. (ex: https://www.saveonfoods.ca)
+
+    let page = undefined;
+
+    const currentFileUrl = new URL(import.meta.url);
+    const directoryPath = path.dirname(currentFileUrl.pathname);
+    const endpoint = `file://${directoryPath}/pages/jim_pattison_group.html`;
+
+    const site = 'https://www.saveonfoods.com';
+    const results = await jimPattisonFoodGroupData(page, endpoint, site);
+    let jsonData = JSON.parse(results.jsonData);
+
+    // 3 products total
+    expect(jsonData.length).toBe(3);
+
+    const firstProduct = jsonData[0];
+    const secondProduct = jsonData[1];
+    const thirdProduct = jsonData[2];
+
+    // test the title, price and link of one of the products
+    // note: the results should also be sorted by price, thats why we see 4.49 as the 2nd item and not the 1st. (4.49, 5.49, 6.49)
+    expect(firstProduct.title).toBe('Pillsbury Pizza Pockets Pepperoni');
+    expect(firstProduct.price).toBe(4.49);
+    expect(firstProduct.link).toBe(`${site}/pepperoni`);
+
+    expect(secondProduct.title).toBe('Pillsbury Pizza Pockets 4 Cheese');
+    expect(secondProduct.price).toBe(5.49);
+    expect(secondProduct.link).toBe(`${site}/4cheese`);
+
+    expect(thirdProduct.title).toBe('Pillsbury Pizza Pockets Supreme Deluxe');
+    expect(thirdProduct.price).toBe(6.49);
+    expect(thirdProduct.link).toBe(`${site}/deluxe`);
   });
 });
